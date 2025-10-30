ff8e698b75df122b64366b86cfe6eda044412f55
Samuel Milhomens <samuel.mil.mar@gmail.com>
2025-10-30 19:17:58 -0300

    Atualizacoes com erros a serem corrigidos

---
+++ app/test/page.tsx
@@ -0,0 +1,13 @@
+import Gemini from "@/utils/gemini"
+
+export default async function Test(){
+    //const ia = new Gemini();
+
+    //ia.responseGenerator();
+
+    return (
+        <div className="min-h-screen bg-[#030712] text-white">
+            <p>Helo world</p>
+        </div>
+    )
+}
